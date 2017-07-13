4d19fffd6b7ce9354c4c6ab7f971fa45c2d571eb
Nirjan Khadka <webdev.nk@gmail.com>
2017-07-13 14:11:51 +0545

    finished testimonials section
    - also finished the blog section

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,7 +1,11 @@
 const NAVICON=document.querySelector(".nav-toggle");
 const NAVMENU=document.querySelector("nav");
 const HEADER=document.querySelector("header");
+const SLIDES=document.querySelectorAll(".testimonial");
+const INDICATORS=document.querySelectorAll(".slide-indicator")
 var navChange=false;
+var slideNo=0;
+var interval;
 NAVICON.onclick=function () {
     this.classList.toggle("fa-bars");
     this.classList.toggle("fa-times");
@@ -23,3 +27,30 @@ NAVICON.onclick=function () {
             navChange=false;
         }
     };
+/** slider **/
+function slideChange(n){
+    SLIDES[slideNo].classList.remove("active");
+    INDICATORS[slideNo].classList.remove("active");
+    slideNo=(n+SLIDES.length)%SLIDES.length;
+    SLIDES[slideNo].classList.add("active");
+    INDICATORS[slideNo].classList.add("active");
+}
+interval=setInterval(nextSlide,15000);
+
+function nextSlide(){ 
+    slideChange(slideNo+1);
+}
+
+/* slider indicators */
+INDICATORS[0].onclick=function () {
+    clearInterval(interval);
+    slideChange(0);
+};
+INDICATORS[1].onclick=function () {
+    clearInterval(interval);
+    slideChange(1);
+};
+INDICATORS[2].onclick=function () {
+    clearInterval(interval);
+    slideChange(2);
+};
